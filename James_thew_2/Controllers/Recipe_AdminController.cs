6fa9e58990eb51a80144908aec957ea43d84fe78
Shahzain Javed <shahzain@aptechnn2.edu>
2023-07-12 18:51:48 +0500

    recipes insert

--- James_thew_2/Controllers/Recipe_AdminController.cs
+++ James_thew_2/Controllers/Recipe_AdminController.cs
@@ -1,7 +1,12 @@
 ﻿using James_thew_2.Models.DB_Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +15,12 @@ namespace James_thew_2.Controllers
     public class Recipe_AdminController : Controller
     {
         james_thew_recipesContext db = new james_thew_recipesContext();
+        IWebHostEnvironment env;
+
+        public Recipe_AdminController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
 
         public IActionResult Categories()
         {
@@ -38,7 +49,46 @@ namespace James_thew_2.Controllers
         }
         public IActionResult Recipes()
         {
+            ViewBag.CategoryId = db.Categories
+                .Select(c => new SelectListItem() { Text = c.CategoryName, Value = c.CategoryId.ToString() })
+                .ToList();
+
+            ViewBag.rec_data = db.Recipes.Include(x=>x.Category).ToList();
+
             return View();
+        }
+        [HttpPost]
+        public IActionResult Recipes(Recipe r , IFormFile r_image ,int free_mem)
+        {
+            if (r_image == null || r_image.Length == 0)
+            {
+                TempData["image_error"] = "Image Is Empty";
+                return RedirectToAction("Recipes");
+            }
+            else
+            {
+                var file_name = Guid.NewGuid() + r_image.FileName;
+                var file_path = Path.Combine(env.WebRootPath, "img/recipes");
+                var final_path = Path.Combine(file_path, file_name);
+
+                var FS = new FileStream(final_path, FileMode.Create);
+
+                r_image.CopyTo(FS);
+                
+                r.RecipeImage = "/img/recipes/" + file_name;
+
+                r.UserId =7;
+                r.CreatedDate = DateTime.Now;
+                r.IsFree = Convert.ToBoolean(free_mem);
+
+                db.Recipes.Add(r);
+                db.SaveChanges();
+
+                FS.Close();
+
+                TempData["recip_succ"] = "Recipe Added Successfully";
+                return RedirectToAction("recipes");
+            }
         }
         public IActionResult Ingredients()
         {
--- James_thew_2/Models/DB_Models/Recipe.cs
+++ James_thew_2/Models/DB_Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -18,9 +19,17 @@ namespace James_thew_2.Models.DB_Models
 
         public int RecipeId { get; set; }
         public int? UserId { get; set; }
+
+        [Required(ErrorMessage ="Title is Required")]
+        [Display(Name ="Recipe Title")]
         public string RecipeTitle { get; set; }
+        [Required(ErrorMessage = "Recipe Main Image is Required")]
+        [Display(Name = "Recipe Main Image")]
         public string RecipeImage { get; set; }
+       
         public int? CategoryId { get; set; }
+       
+        [Display(Name = "Set Recipe Free or MemberShip Only")]
         public bool? IsFree { get; set; }
         public DateTime? CreatedDate { get; set; }
 
